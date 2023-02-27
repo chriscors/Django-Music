6330d71f5f78802004f8299e11ddf8064b7e12c5
chriscors <70537376+chriscors@users.noreply.github.com>
2023-02-27 15:57:20 -0500

    Adds home page functionality

--- albums/views.py
+++ albums/views.py
@@ -3,5 +3,5 @@ from .models import Album
 
 
 def album_list(request):
-    albums = Album.objects.order_by('creation_date')
-    render(request, 'blog/album_list.html', {'albums', albums})
+    albums = Album.objects.all().order_by('creation_date')
+    return render(request, 'albums/album_list.html', {'albums': albums})
--- djando_music/urls.py
+++ djando_music/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from albums import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('__debug__/', include('debug_toolbar.urls')),
+    path('', views.album_list, name='album_list'),
 ]
