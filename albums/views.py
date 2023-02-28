366078be77a594d584cbc86faeb8dd41add40f42
chriscors <70537376+chriscors@users.noreply.github.com>
2023-02-28 10:59:44 -0500

    Fixes views errors; starts building HTML template

--- albums/views.py
+++ albums/views.py
@@ -3,5 +3,5 @@ from .models import Album
 
 
 def album_list(request):
-    albums = Album.objects.all().order_by('creation_date')
+    albums = Album.objects.all().order_by("title")
     return render(request, 'albums/album_list.html', {'albums': albums})
